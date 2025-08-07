3d113f9f35237f32b2f79e8d57b6670b5dffc605
mdsameersakib <mdsameersakib@gmail.com>
2025-08-07 20:47:09 +0600

    Trying to show them as married

--- frontend/src/components/MarriageNode.js
+++ frontend/src/components/MarriageNode.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Handle, Position } from 'reactflow';
 
-// This simple component renders the small circle between spouses.
-export default function MarriageNode({ style }) {
+export default function MarriageNode() {
   return (
-    <div 
-      style={style} 
-      className="absolute w-5 h-5 bg-gray-500 rounded-full border-2 border-white shadow-md z-0"
-      title="Marriage"
-    >
-    </div>
+    <>
+      {/* Target handles for the spouses to connect to */}
+      <Handle type="target" position={Position.Left} id="left" className="!bg-transparent !border-none" />
+      <Handle type="target" position={Position.Right} id="right" className="!bg-transparent !border-none" />
+      {/* Source handle for the children to connect from */}
+      <Handle type="source" position={Position.Bottom} id="bottom" className="!bg-gray-500" />
+
+      <div 
+        className="w-6 h-6 bg-gray-500 rounded-full border-2 border-white shadow-md"
+        title="Marriage"
+      >
+      </div>
+    </>
   );
 }
